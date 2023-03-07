c84f1c2f7bd78114b5a65775bc11b16d9dcf73d0
Elena Sergienko <marketing.seo.web2020@gmail.com>
2023-03-07 19:32:13 +0300

    Add code

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,8 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
 //  Напишите программу создания небольшого словаря сленговых программерских выражений, чтобы она
@@ -39,7 +44,50 @@ public class Main {
 //  Не найдено
 //  код, который нужен, чтобы исправить несовершенство ранее написанного кода
 
-  public static void main(String[] args) {
+  public static Map<String, String> readDictionaryFromFile(File inputFile) throws IOException {
+    Map<String, String> dictionary = new HashMap<>();
+//    try {
+    BufferedReader br = new BufferedReader(new FileReader(inputFile)); // ошибка, если файла нет
 
+    int n = Integer.parseInt(br.readLine()); // ошибка, только если файл неправильный
+    for (int i = 0; i < n; ++i) { // прочитать n раз
+      String line = br.readLine(); // "Змея: язык программирования Python"
+      int spaceIndex = line.indexOf(':'); // индекс разделителя (4)
+      // с начала и до двоеточия - слово
+      String word = line.substring(0, spaceIndex); // "Змея" // ошибка, если файл неправильный
+      // строка с определением - от "после двоеточия и пробела" до конца
+      String definition = line.substring(spaceIndex + 2); //
+      dictionary.put(word, definition); // записываем в словарь
+    }
+    br.close();
+//    } catch (FileNotFoundException e) {
+//      System.err.println("Файл не найден: " + e.getMessage());
+//    } catch (IndexOutOfBoundsException e) {
+//      // если при поиске пробела получили -1 и подставили его в substring
+//      System.err.println("Ошибка в файле");
+//    }
+    return dictionary;
   }
+
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    Map<String, String> dictionary = readDictionaryFromFile(new File("res/dict.txt"));
+
+    int m = Integer.parseInt(br.readLine());
+    ArrayList<String> wordList = new ArrayList<>();
+    for (int i = 0; i < m; ++i) { // прочитать m раз
+      wordList.add(i + 1, br.readLine());
+    }
+
+    for (int x = 0; x < m; ++x) { // проверить m раз
+      if (dictionary.containsKey(wordList.get(x))) {
+        System.out.println(dictionary.get(wordList.get(x)));
+      }
+      System.out.println("Не найдено");
+    }
+  }
+
+
 }
